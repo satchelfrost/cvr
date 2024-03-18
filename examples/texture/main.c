288e718b7a4d874f07518d4a217ec2ae0d23b5b0
Reese Gallagher <reese.gallagher@infinitasinc.com>
2024-03-18 17:34:54 -0400

    Texture example, and run after build for jpg image, temporary fix

---
+++ examples/texture/main.c
@@ -0,0 +1,46 @@
+#include "cvr.h"
+#include "ext/nob.h"
+
+
+int main()
+{
+    Camera camera = {
+        .position   = {5.0f, 5.0f, 5.0f},
+        .target     = {0.0f, 0.0f, 0.0f},
+        .up         = {0.0f, 1.0f, 0.0f},
+        .fovy       = 45.0f,
+        .projection = PERSPECTIVE,
+    };
+
+    const int width  = 500;
+    const int height = 500;
+    init_window(width, height, "Load texture");
+
+    const char *img_name = "res/statue.jpg";
+    Image img = load_image(img_name);
+    if (!img.data) {
+        nob_log(NOB_ERROR, "image %s could not be loaded", img_name);
+        return 1;
+    } else {
+        nob_log(NOB_INFO, "image %s was successfully loaded", img_name);
+        nob_log(NOB_INFO, "    (height, width) = (%d, %d)", img.height, img.width);
+        nob_log(NOB_INFO, "    image size = %d bytes", img.height * img.width * 4);
+    }
+
+    // Texture tex = load_texture_from_image(img);
+    unload_image(img);
+
+    while(!window_should_close()) {
+        begin_drawing(RED);
+        begin_mode_3d(camera);
+
+        // draw_texture(texture, width / 2, height / 2, PINK);
+
+        end_mode_3d();
+        end_drawing();
+    }
+
+    // unload_texture(tex);
+    close_window();
+    return 0;
+}
---
+++ examples/texture/shaders/shader.vert
@@ -0,0 +1,24 @@
+#version 450
+
+layout(push_constant) uniform constants
+{
+    mat4 mvp;
+} push_const;
+
+layout(binding = 0) uniform UniformBufferObject
+{
+    mat4 model;
+    mat4 view;
+    mat4 proj;
+} ubo;
+
+layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec3 inColor;
+
+layout(location = 0) out vec3 fragColor;
+
+void main()
+{
+    gl_Position = push_const.mvp * vec4(inPosition, 1.0);
+    fragColor = inColor;
+}
