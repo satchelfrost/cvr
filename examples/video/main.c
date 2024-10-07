a66897ecaf3e4b75b72310c00dc6a64bd397a363
Reese Gallagher <re325479@ucf.edu.com>
2024-10-07 18:13:34 -0400

    broken

--- examples/video/main.c
+++ examples/video/main.c
@@ -34,6 +34,9 @@ const char *video_names[] = {
 typedef struct {
     Vk_Texture planes[VIDEO_IDX_COUNT * VIDEO_PLANE_COUNT];
     Vk_Buffer stg_buffs[VIDEO_IDX_COUNT * VIDEO_PLANE_COUNT];
+    Image img[VIDEO_IDX_COUNT * VIDEO_PLANE_COUNT];
+    plm_t *plms[VIDEO_IDX_COUNT];
+    float aspects[VIDEO_IDX_COUNT];
     VkDescriptorSet ds_sets[VIDEO_IDX_COUNT];
     VkDescriptorPool ds_pool;
     VkDescriptorSetLayout ds_layout;
@@ -79,33 +82,31 @@ bool setup_ds_pool()
 
 bool setup_ds_sets()
 {
-    // for (size_t i = 0; i < VIDEO_IDX_COUNT; i++) {
-    for (size_t i = 0; i < 1; i++) {
-        VkDescriptorSetAllocateInfo alloc = {
-            DS_ALLOC(&video_textures.ds_layout, 1, video_textures.ds_pool)
-        };
-        if (!vk_alloc_ds(alloc, &video_textures.ds_sets[i])) return false;
+    VkDescriptorImageInfo y_img_info  = {.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL};
+    VkDescriptorImageInfo cb_img_info = {.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL};
+    VkDescriptorImageInfo cr_img_info = {.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL};
+    VkWriteDescriptorSet writes[] = {
+        {DS_WRITE_IMG(0, COMBINED_IMAGE_SAMPLER, NULL, &y_img_info)},
+        {DS_WRITE_IMG(1, COMBINED_IMAGE_SAMPLER, NULL, &cb_img_info)},
+        {DS_WRITE_IMG(2, COMBINED_IMAGE_SAMPLER, NULL, &cr_img_info)},
+    };
+    VkDescriptorSetLayout layouts[] = {video_textures.ds_layout, video_textures.ds_layout, video_textures.ds_layout};
+    VkDescriptorSetAllocateInfo alloc = {
+        // DS_ALLOC(&video_textures.ds_layout, 3, video_textures.ds_pool)
+        DS_ALLOC(layouts, 3, video_textures.ds_pool)
+    };
+    if (!vk_alloc_ds(alloc, video_textures.ds_sets)) return false;
 
-        VkDescriptorImageInfo y_img_info = {
-            .imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-            .imageView   = video_textures.planes[VIDEO_PLANE_Y + i * VIDEO_IDX_COUNT].view,
-            .sampler     = video_textures.planes[VIDEO_PLANE_Y + i * VIDEO_IDX_COUNT].sampler,
-        };
-        VkDescriptorImageInfo cb_img_info = {
-            .imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-            .imageView   = video_textures.planes[VIDEO_PLANE_CB + i * VIDEO_IDX_COUNT].view,
-            .sampler     = video_textures.planes[VIDEO_PLANE_CB + i * VIDEO_IDX_COUNT].sampler,
-        };
-        VkDescriptorImageInfo cr_img_info = {
-            .imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-            .imageView   = video_textures.planes[VIDEO_PLANE_CR + i * VIDEO_IDX_COUNT].view,
-            .sampler     = video_textures.planes[VIDEO_PLANE_CR + i * VIDEO_IDX_COUNT].sampler,
-        };
-        VkWriteDescriptorSet writes[] = {
-            {DS_WRITE_IMG(0, COMBINED_IMAGE_SAMPLER, video_textures.ds_sets[i], &y_img_info)},
-            {DS_WRITE_IMG(1, COMBINED_IMAGE_SAMPLER, video_textures.ds_sets[i], &cb_img_info)},
-            {DS_WRITE_IMG(2, COMBINED_IMAGE_SAMPLER, video_textures.ds_sets[i], &cr_img_info)},
-        };
+    for (size_t i = 0; i < VIDEO_IDX_COUNT; i++) {
+        y_img_info.imageView  = video_textures.planes[VIDEO_PLANE_Y + i *  VIDEO_PLANE_COUNT].view;
+        y_img_info.sampler    = video_textures.planes[VIDEO_PLANE_Y + i *  VIDEO_PLANE_COUNT].sampler;
+        cb_img_info.imageView = video_textures.planes[VIDEO_PLANE_CB + i * VIDEO_PLANE_COUNT].view,
+        cb_img_info.sampler   = video_textures.planes[VIDEO_PLANE_CB + i * VIDEO_PLANE_COUNT].sampler,
+        cr_img_info.imageView = video_textures.planes[VIDEO_PLANE_CR + i * VIDEO_PLANE_COUNT].view,
+        cr_img_info.sampler   = video_textures.planes[VIDEO_PLANE_CR + i * VIDEO_PLANE_COUNT].sampler,
+        writes[0].dstSet = video_textures.ds_sets[i];
+        writes[1].dstSet = video_textures.ds_sets[i];
+        writes[2].dstSet = video_textures.ds_sets[i];
         vk_update_ds(NOB_ARRAY_LEN(writes), writes);
     }
 
@@ -207,48 +208,57 @@ int main()
     // set_target_fps(60);
 
     /* create a texture from video */
-    const char *file_name = "res/suite_e_snippet.mpg";
+    // const char *file_name = "res/suite_e_snippet.mpg";
     // const char *file_name = "res/bjork-all-is-full-of-love.mpg";
-	plm_t *plm = plm_create_with_filename(file_name);
-	if (!plm) {
-		nob_log(NOB_ERROR, "could not open file %s", file_name);
-		return 1;
-	}
-    // plm_set_loop(plm, TRUE);
-    plm_set_audio_enabled(plm, FALSE);
-    Image img = {
-        .format = VK_FORMAT_R8_UNORM,
-        .width  = plm_get_width(plm),
-        .height = plm_get_height(plm),
-    };
-    float aspect = (float)img.width / img.height;
-
-    nob_log(NOB_INFO, "first frame %s was successfully loaded from", file_name);
-    nob_log(NOB_INFO, "    (height, width) = (%d, %d)", img.height, img.width);
-    nob_log(NOB_INFO, "    image size in memory = %d bytes", img.height * img.width * 4);
+    for (size_t i = 0; i < NOB_ARRAY_LEN(video_names); i++) {
+        const char *file_name = nob_temp_sprintf("res/%s_snippet.mpg", video_names[i]);
+        plm_t *plm = plm_create_with_filename(file_name);
+        if (!plm) {
+            nob_log(NOB_ERROR, "could not open file %s", file_name);
+            return 1;
+        } else {
+            video_textures.plms[i] = plm;
+            // plm_set_loop(plm, TRUE);
+            plm_set_audio_enabled(plm, FALSE);
+            Image img = {
+                .format = VK_FORMAT_R8_UNORM,
+                .width  = plm_get_width(plm),
+                .height = plm_get_height(plm),
+            };
+            video_textures.aspects[i] = img.width / img.height;
+            nob_log(NOB_INFO, "first frame %s was successfully loaded from", file_name);
+            nob_log(NOB_INFO, "    (height, width) = (%d, %d)", img.height, img.width);
+            nob_log(NOB_INFO, "    image size in memory = %d bytes", img.height * img.width * 4);
+        }
+    }
 
     /* decode one frame and use the luminance to create a texture */
-    plm_frame_t *frame = plm_decode_video(plm);
-    img.data   = frame->y.data;
-    img.width  = frame->y.width;
-    img.height = frame->y.height;
-    nob_log(NOB_INFO, "y height %zu width %zu", frame->y.width, frame->y.height);
-    if (!init_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_Y)) return 1;
-    img.data   = frame->cb.data;
-    img.width  = frame->cb.width;
-    img.height = frame->cb.height;
-    nob_log(NOB_INFO, "cb height %zu width %zu", frame->cb.width, frame->cb.height);
-    if (!init_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CB)) return 1;
-    img.data   = frame->cr.data;
-    img.width  = frame->cr.width;
-    img.height = frame->cr.height;
-    nob_log(NOB_INFO, "cr height %zu width %zu", frame->cr.width, frame->cr.height);
-    if (!init_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CR)) return 1;
+    for (size_t i = 0; i < VIDEO_IDX_COUNT; i++) {
+        Image img = {.format = VK_FORMAT_R8_UNORM};
+        plm_frame_t *frame = plm_decode_video(video_textures.plms[i]);
+        img.data   = frame->y.data;
+        img.width  = frame->y.width;
+        img.height = frame->y.height;
+        nob_log(NOB_INFO, "y height %zu width %zu", frame->y.width, frame->y.height);
+        if (!init_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_Y)) return 1;
+        img.data   = frame->cb.data;
+        img.width  = frame->cb.width;
+        img.height = frame->cb.height;
+        nob_log(NOB_INFO, "cb height %zu width %zu", frame->cb.width, frame->cb.height);
+        if (!init_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CB)) return 1;
+        img.data   = frame->cr.data;
+        img.width  = frame->cr.width;
+        img.height = frame->cr.height;
+        nob_log(NOB_INFO, "cr height %zu width %zu", frame->cr.width, frame->cr.height);
+        if (!init_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CR)) return 1;
+    }
 
     /* setup descriptors */
     if (!setup_ds_layout()) return 1;
     if (!setup_ds_pool())   return 1;
     if (!setup_ds_sets())   return 1;
+    return 1;
+
 
     /* setup the graphics pipeline */
     VkPushConstantRange pk_range = {.stageFlags = VK_SHADER_STAGE_VERTEX_BIT, .size = sizeof(float16)};
@@ -267,28 +277,28 @@ int main()
 
         /* decode the next frame */
         if (vid_update_time > 1.0f / 30.0f) {
-            plm_frame_t *frame = plm_decode_video(plm);
-            if (!frame) {
-                nob_log(NOB_INFO, "frame missing");
-                return 1;
+            for (size_t i = 0; i < VIDEO_IDX_COUNT; i++) {
+                plm_frame_t *frame = plm_decode_video(video_textures.plms[i]);
+                if (!frame) {
+                    playback_finished = true;
+                    nob_log(NOB_INFO, "playback finished!");
+                    return 1;
+                }
+
+                Image img = {.format = VK_FORMAT_R8_UNORM};
+                img.data   = frame->y.data;
+                img.width  = frame->y.width;
+                img.height = frame->y.height;
+                if (!update_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_Y)) return 1;
+                img.data   = frame->cb.data;
+                img.width  = frame->cb.width;
+                img.height = frame->cb.height;
+                if (!update_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CB)) return 1;
+                img.data   = frame->cr.data;
+                img.width  = frame->cr.width;
+                img.height = frame->cr.height;
+                if (!update_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CR)) return 1;
             }
-            if (!frame) {
-                playback_finished = true;
-                nob_log(NOB_INFO, "playback finished!");
-                continue;
-            }
-            img.data   = frame->y.data;
-            img.width  = frame->y.width;
-            img.height = frame->y.height;
-            if (!update_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_Y)) return 1;
-            img.data   = frame->cb.data;
-            img.width  = frame->cb.width;
-            img.height = frame->cb.height;
-            if (!update_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CB)) return 1;
-            img.data   = frame->cr.data;
-            img.width  = frame->cr.width;
-            img.height = frame->cr.height;
-            if (!update_video_texture(img, VIDEO_IDX_SUITE_E, VIDEO_PLANE_CR)) return 1;
 
             vid_update_time = 0.0f;
         }
@@ -296,25 +306,22 @@ int main()
         /* drawing */
         begin_drawing(BLUE);
             begin_mode_3d(camera);
-                push_matrix();
-                    scale(aspect, 1.0f, 1.0f);
-                    if (!draw(video_textures.gfx_pl, video_textures.pl_layout, video_textures.ds_sets[0], SHAPE_QUAD))
+                for (size_t i = 0; i < VIDEO_IDX_COUNT; i++) {
+                    push_matrix();
+                    scale(video_textures.aspects[i], 1.0f, 1.0f);
+                    translate(i, i, 0.0f);
+                    if (!draw(video_textures.gfx_pl, video_textures.pl_layout, video_textures.ds_sets[i], SHAPE_QUAD))
                         return 1;
-                pop_matrix();
-                translate(0.0f, 1.0f, 0.0f);
-                scale(0.2f, 0.2f, 0.2f);
-                rotate_y(get_time());
-                draw_shape(SHAPE_CUBE);
+                    pop_matrix();
+                }
             end_mode_3d();
         end_drawing();
     }
 
-    /* cleanup host */
-    plm_destroy(plm);
-
     /* cleanup device */
     wait_idle();
     for (size_t i = 0; i < VIDEO_IDX_COUNT; i++) {
+        plm_destroy(video_textures.plms[i]);
         for (size_t j = 0; j < VIDEO_PLANE_COUNT; j++) {
             vk_buff_destroy(video_textures.stg_buffs[j + i * VIDEO_PLANE_COUNT]);
             vk_unload_texture2(video_textures.planes[j + i * VIDEO_PLANE_COUNT]);
