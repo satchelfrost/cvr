3113eca74b0c70cb62afb18705c2ca07fa568bd8
Reese Gallagher <re325479@ucf.edu.com>
2025-01-29 11:01:55 -0500

    basic cube

--- examples/mixed-raster/main.c
+++ examples/mixed-raster/main.c
@@ -3,9 +3,20 @@
 int main()
 {
     init_window(800, 600, "mixed raster");
+
+    Camera camera = {
+        .position   = {5.0f, 5.0f, 5.0f},
+        .target     = {0.0f, 0.0f, 0.0f},
+        .up         = {0.0f, 1.0f, 0.0f},
+        .fovy       = 45.0f,
+        .projection = PERSPECTIVE,
+    };
     
     while (!window_should_close()) {
         begin_drawing(BLUE);
+            begin_mode_3d(camera);
+                draw_shape(SHAPE_CUBE);
+            end_mode_3d();
         end_drawing();
     }
 
